98d3bca07ca538518032bcf928eb72aa2c88697d
Qoddrysdaim <filoder@ya.ru>
2022-02-22 19:00:25 +0300

    Refactoring Json Parser

--- src/json_parser/JsonParser.cpp
+++ src/json_parser/JsonParser.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "JsonParser.h"
 
-JsonParser::JsonParser() {
+
+
+Json::Parser::Parser() {
     auto &&error_code = parser.allocate(
             0x1000,    // Размер внутреннего буфера.
             0x20);    // Максимальная глубина вложенности JSON
@@ -10,8 +12,8 @@ JsonParser::JsonParser() {
     }
 }
 
-bool
-JsonParser::follow_path(simdjson::simdjson_result<simdjson::dom::element> &frame,
+void
+Json::Parser::follow_path(Json::Parser::Error &ec, simdjson::simdjson_result<simdjson::dom::element> &frame,
                         const std::vector<std::variant<size_t, std::string>> &path) {
     frame = current_json;
     for (const auto& jsonStep : path) {
@@ -27,123 +29,48 @@ JsonParser::follow_path(simdjson::simdjson_result<simdjson::dom::element> &frame
 
         // Если неправильный тип (не size_t или std::string)
         } else {
-            throw(std::runtime_error("Invalid type of JSON field (not std::string or size_t)"));
+            ec.ec = UNEXPECTED_TYPE_IN_PATH;
+            ec.message = "Unexpected type in path (not std::string or size_t)";
         }
 
         // проверка, получено ли поле или элемент массива
-        if (simdjson::SUCCESS != frame.error())
+        if (simdjson::SUCCESS != frame.error()) {
             // если не получено, возникла ошибка
-            return false;
+            ec.ec = NOT_FOUND;
+            ec.message = "Non-existent JSON step in path";
+        }
     }
     // если всё ОК и весь путь вглубь json пройден
-    return true;
+    ec.ec = SUCCESS;
 }
 
-bool JsonParser::load_json_from_string(const std::string_view &json_frames) {
+void Json::Parser::load_json_from_string(Error &ec, const std::string_view &json_frames) {
     current_json = parser.parse(
             json_frames.data(),        // Указатель на разбираемый фрейм.
             json_frames.size(),        // Размер фрейма.
             false);
     if (simdjson::SUCCESS == current_json.error()) {
-        return true;
+        ec.ec = SUCCESS;
+    } else {
+        ec.ec = FAILED_MEMORY_ALLOCATE;
+        ec.message = "Memory for JSON parser doesn't allocated";
     }
-    return false;
 }
 
-bool JsonParser::load_json_from_file(const std::basic_string<char>& path_to_file) {
+void Json::Parser::load_json_from_file(Error &ec, const std::basic_string<char> &path_to_file) {
     current_json = parser.load(path_to_file);
     if (simdjson::SUCCESS == current_json.error()) {
-        return true;
+        ec.ec = SUCCESS;
+    } else {
+        ec.ec = FAILED_PARSING;
+        ec.message = "Failed to parsing";
     }
-    return false;
 }
 
-bool JsonParser::change_root_element(const std::vector<std::variant<size_t, std::string>> &path) {
-    return follow_path(current_json, path);
+void Json::Parser::change_root_element(Error &ec, const std::vector<std::variant<size_t, std::string>> &path) {
+    return follow_path(ec, current_json, path);
 }
 
-bool
-JsonParser::parse_string(std::string &result, const std::vector<std::variant<size_t, std::string>> &path) {
-    simdjson::simdjson_result<simdjson::dom::element> element;
-    // проследую пути, вглубь JSON
-    // в результате ожидаю получить конкретную строку внутри JSON
-    if (follow_path(element, path)) {
 
-        // Пытаюсь преобразовать полученные элемент к строке
-        auto retrieved_string = element.get_string();
 
-        // если получилось преобразовать к строке...
-        if (simdjson::SUCCESS == retrieved_string.error()) {
-            // Записываю полученную строку в ответ
-            result = retrieved_string.value();
-            // true - т.к. строка успешно получена
-            return true;
-        }
-    }
-    // false - не удалось получить строку
-    return false;
-}
 
-bool
-JsonParser::parse_double(double &result, const std::vector<std::variant<size_t, std::string>> &path) {
-    simdjson::simdjson_result<simdjson::dom::element> element;
-    // проследую пути, вглубь JSON
-    // в результате ожидаю получить конкретный double внутри JSON
-    if (follow_path(element, path)) {
-
-        // Пытаюсь преобразовать полученный элемент к double
-        auto retrieved_double = element.get_double();
-
-        // Если получилось преобразовать к double...
-        if (simdjson::SUCCESS == retrieved_double.error()) {
-            // Записываю полученный double в ответ
-            result = retrieved_double.value();
-            // true - т.к. double успешно получен
-            return true;
-        }
-    }
-    // false - не удалось получить double
-    return false;
-}
-
-bool JsonParser::parse_int64_t(int64_t &result, const std::vector<std::variant<size_t, std::string>> &path) {
-    simdjson::simdjson_result<simdjson::dom::element> element;
-    // проследую пути, вглубь JSON
-    // в результате ожидаю получить конкретный int64 внутри JSON
-    if (follow_path(element, path)) {
-
-        // Пытаюсь преобразовать полученный элемент к int64
-        auto retrieved_int64 = element.get_int64();
-
-        // Если получилось преобразовать к int64...
-        if (simdjson::SUCCESS == retrieved_int64.error()) {
-            // Записываю полученный int64 в ответ
-            result = retrieved_int64.value();
-            // true - т.к. int64 успешно получен
-            return true;
-        }
-    }
-    // false - не удалось получить int64
-    return false;
-}
-
-bool JsonParser::parse_bool(bool &result, const std::vector<std::variant<size_t, std::string>> &path) {
-    simdjson::simdjson_result<simdjson::dom::element> element;
-    // проследую пути, вглубь JSON
-    // в результате ожидаю получить конкретный bool внутри JSON
-    if (follow_path(element, path)) {
-
-        // Пытаюсь преобразовать полученный элемент к bool
-        auto retrieved_bool = element.get_bool();
-
-        // Если получилось преобразовать к bool...
-        if (simdjson::SUCCESS == retrieved_bool.error()) {
-            // Записываю полученный bool в ответ
-            result = retrieved_bool.value();
-            // true - т.к. bool успешно получен
-            return true;
-        }
-    }
-    // false - не удалось получить bool
-    return false;
-}
--- src/json_parser/JsonParser.h
+++ src/json_parser/JsonParser.h
@@ -7,74 +7,113 @@
 #include "../KucoinAPI/src/KucoinDataclasses.h"
 #include "Exceptions.h"
 
+namespace Json {
+
+    struct Error;
 
 /// @brief Парсер JSON, настраивается на файл или строку и получает поля из неё. Работает в режиме dom.
 ///
-/// @details Использует simdjson dom parser. Можно менять JSON, который будет парсить JsonParser, не создавая нового
+/// @details Использует simdjson dom parser. Можно менять JSON, который будет парсить Parser, не создавая нового
 /// объекта класса.
-class JsonParser {
-private:
-    simdjson::dom::parser parser;
-    simdjson::simdjson_result<simdjson::dom::element> current_json;
+    class Parser {
+    private:
+        simdjson::dom::parser parser;
+        simdjson::simdjson_result<simdjson::dom::element> current_json;
 
-    /// @brief Получить элемент внутри JSON
-    bool follow_path(simdjson::simdjson_result<simdjson::dom::element> &frame, const std::vector<std::variant<size_t, std::string>>& path);
+    public:
 
-public:
+        enum error_code {
+            SUCCESS,
+            FAILED_MEMORY_ALLOCATE,
+            FAILED_PARSING,
+            NOT_FOUND,
+            INVALID_TYPE,
+            UNEXPECTED_TYPE_IN_PATH
+        };
 
-    /// @brief Конструктор, инициализирует парсер dom
-    JsonParser();
+        struct Error {
+            error_code ec;
+            std::string message;
+        };
 
-    /// @brief Установить JSON, который будет парсить объект из строки
-    ///
-    /// @param json_frames строка JSON
-    bool load_json_from_string(const std::string_view &json_frames);
+        /// @brief Конструктор, инициализирует парсер dom
+        Parser();
 
-    /// @brief Установить JSON, cпарсить объект из файла
-    ///
-    /// @param path_to_file путь до файла
-    bool load_json_from_file(const std::basic_string<char>& path_to_file);
 
-    /// @brief Сделать корневым элементом JSON другой элемент внутри текущего
-    /// т.е. при поиске полей поиск будет вестись от нового корневого элемента
-    bool change_root_element(const std::vector<std::variant<size_t, std::string>>& path);
+        /// @brief Получить элемент внутри JSON
+        ///
+        /// @param ec структура типа Parser::Error, в которую будет записана ошибка, или SUCCESS в случае успеха
+        /// содержит два поля: ec и message. ec содержит значение enum Parser::error_code,  message содержит std::string
+        /// с текстом ошибки
+        /// @param frame элемент json
+        /// @param path путь до поля, включающий название поля. Задавать в формате {"field1", "array", 0, "result_field"}
+        /// т.е. массив из std::string и size_t. std::string это название поля, size_t это индекс в массиве
+        void follow_path(Error &ec, simdjson::simdjson_result<simdjson::dom::element> &frame,
+                         const std::vector<std::variant<size_t, std::string>> &path);
+        /// @brief Установить JSON, который будет парсить объект из строки
+        ///
+        /// @param ec структура типа Parser::Error, в которую будет записана ошибка, или SUCCESS в случае успеха
+        /// содержит два поля: ec и message. ec содержит значение enum Parser::error_code,  message содержит std::string
+        /// с текстом ошибки
+        /// @param json_frames строка JSON
+        void load_json_from_string(Error &ec, const std::string_view &json_frames);
 
-    /// @brief Получить поле, содержащее значение типа std::string
-    ///
-    /// @param result переменная типа std::string, в которую будет помещен результат. Если поле не найдено, переменная не изменится
-    /// @param path путь до поля, включающий название поля. Задавать в формате {"field1", "array", 0, "result_field"}
-    /// т.е. массив из std::string и size_t. std::string это название поля, size_t это индекс в массиве
-    ///
-    /// @result true, если успешно получено поле. false, если поле не получено (произошла ошибка)
-    bool parse_string(std::string &target_string, const std::vector<std::variant<size_t, std::string>>& path);
+        /// @brief Установить JSON, cпарсить объект из файла
+        ///
+        /// @param ec структура типа Parser::Error, в которую будет записана ошибка, или SUCCESS в случае успеха
+        /// содержит два поля: ec и message. ec содержит значение enum Parser::error_code,  message содержит std::string
+        /// с текстом ошибки
+        /// @param path_to_file путь до файла
+        void load_json_from_file(Error &ec, const std::basic_string<char> &path_to_file);
 
-    /// @brief Получить поле, содержащее значение типа double
-    ///
-    /// @param result переменная типа double, в которую будет помещен результат. Если поле не найдено, переменная не изменится
-    /// @param path путь до поля, включающий название поля. Задавать в формате {"field1", "array", 0, "result_field"}
-    /// т.е. массив из std::string и size_t. std::string это название поля, size_t это индекс в массиве
-    ///
-    /// @result true, если успешно получено поле. false, если поле не получено (произошла ошибка)
-    bool parse_double(double &result, const std::vector<std::variant<size_t, std::string>> &path);
+        /// @brief Сделать корневым элементом JSON другой элемент внутри текущего
+        /// т.е. при поиске полей поиск будет вестись от нового корневого элемента
+        ///
+        /// @param ec структура типа Parser::Error, в которую будет записана ошибка, или SUCCESS в случае успеха
+        /// содержит два поля: ec и message. ec содержит значение enum Parser::error_code,  message содержит std::string
+        /// с текстом ошибки
+        /// @param path путь до поля, включающий название поля. Задавать в формате {"field1", "array", 0, "result_field"}
+        /// т.е. массив из std::string и size_t. std::string это название поля, size_t это индекс в массиве
+        void change_root_element(Error &ec, const std::vector<std::variant<size_t, std::string>> &path);
 
-    /// @brief Получить поле, содержащее значение типа long
-    ///
-    /// @param result переменная типа long, в которую будет помещен результат. Если поле не найдено, переменная не изменится
-    /// @param path путь до поля, включающий название поля. Задавать в формате {"field1", "array", 0, "result_field"}
-    /// т.е. массив из std::string и size_t. std::string это название поля, size_t это индекс в массиве
-    ///
-    /// @result true, если успешно получено поле. false, если поле не получено (произошла ошибка)
-    bool parse_int64_t(int64_t &result, const std::vector<std::variant<size_t, std::string>> &path);
+        /// @brief Получить поле, которое находится внутри JSON
+        /// Для получения строки указать тип <const char *>
+        /// @param ec структура типа Parser::Error, в которую будет записана ошибка, или SUCCESS в случае успеха
+        /// содержит два поля: ec и message. ec содержит значение enum Parser::error_code,  message содержит std::string
+        /// с текстом ошибки
+        /// @param path путь до поля, включающий название поля. Задавать в формате {"field1", "array", 0, "result_field"}
+        /// т.е. массив из std::string и size_t. std::string это название поля, size_t это индекс в массиве
+        ///
+        /// @result переменная типа std::string, полученная из JSON
+        template<typename Type>
+        Type parse_field(Error &ec, const std::vector<std::variant<size_t, std::string>> &path) {
+            simdjson::simdjson_result<simdjson::dom::element> element;
+            // проследую пути, вглубь JSON
+            // в результате ожидаю получить конкретное поле внутри JSON
+            follow_path(ec, element, path);
 
-    /// @brief Получить поле, содержащее значение типа bool
-    ///
-    /// @param result переменная типа bool, в которую будет помещен результат. Если поле не найдено, переменная не изменится
-    /// @param path путь до поля, включающий название поля. Задавать в формате {"field1", "array", 0, "result_field"}
-    /// т.е. массив из std::string и size_t. std::string это название поля, size_t это индекс в массиве
-    ///
-    /// @result true, если успешно получено поле. false, если поле не получено (произошла ошибка)
-    bool parse_bool(bool &result, const std::vector<std::variant<size_t, std::string>> &path);
-};
+            if (ec.ec == SUCCESS) {
 
+                // Пытаюсь преобразовать полученное поле к целевому типу
+                auto retrieved_value = element.template get<Type>();
+
+
+                // если получилось преобразовать к целевому типу...
+                if (simdjson::SUCCESS == retrieved_value.error()) {
+                    ec.ec = SUCCESS;
+                    // Возвращаю полученное значение
+                    return retrieved_value.value();
+                } else {
+                    ec.ec = INVALID_TYPE;
+                    ec.message = "Failed converting JSON field";
+                }
+            }
+            // не удалось получить поле
+            return {};
+        }
+    };
+
+
+}
 
 #endif //KUCOIN_GATEWAY_JSONPARSER_H
